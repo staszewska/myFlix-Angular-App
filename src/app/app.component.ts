afddbb47b59ddd9be6c418694dac9bb2bb4912f4
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-07-18 14:53:38 +0200

    add openMoviesDialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -31,6 +32,12 @@ export class AppComponent {
 
     dialogRef.afterClosed().subscribe(() => {
       console.log(`Dialog closed`);
+    });
+  }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      // width: '500px',
     });
   }
 }
