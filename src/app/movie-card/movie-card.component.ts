8205d75884f8354d932859e365db8585db577511
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-07-18 14:56:18 +0200

    implement a function to fetch all the movies

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrl: './movie-card.component.scss',
+})
+export class MovieCardComponent {
+  //movies returned from the API
+  movies: any[] = [];
+  constructor(public fetchApiData: FetchApiDataService) {}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  //function to get all movies from the API
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+}
