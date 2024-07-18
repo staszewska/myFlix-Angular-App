dc6cf30037acd7ca75eac3388090685516c48765
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-07-18 15:43:20 +0200

    adjust app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -12,32 +9,4 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 export class AppComponent {
   readonly dialog = inject(MatDialog);
   title = 'myFlix-Angular-v2';
-
-  openUserRegistrationDialog(): void {
-    const dialogRef = this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '350px',
-    });
-
-    dialogRef.afterClosed().subscribe(() => {
-      console.log(`Dialog closed`);
-    });
-  }
-
-  openUserLoginDialog(): void {
-    const dialogRef = this.dialog.open(UserLoginFormComponent, {
-      // Assigning the dialog a width
-      width: '350px',
-    });
-
-    dialogRef.afterClosed().subscribe(() => {
-      console.log(`Dialog closed`);
-    });
-  }
-
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      // width: '500px',
-    });
-  }
 }
