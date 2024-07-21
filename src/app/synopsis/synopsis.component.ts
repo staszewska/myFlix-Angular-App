b57bb874086b2cb2c5b8865bbe900948035129c2
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-07-21 19:41:46 +0200

    implement logic

---
+++ src/app/synopsis/synopsis.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-synopsis',
+  templateUrl: './synopsis.component.html',
+  styleUrl: './synopsis.component.scss',
+})
+export class SynopsisComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<SynopsisComponent>,
+    public fetchSynopsis: FetchApiDataService,
+    @Inject(MAT_DIALOG_DATA) public data
+  ) {
+    console.log(data);
+  }
+
+  ngOnInit(): void {}
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+}
