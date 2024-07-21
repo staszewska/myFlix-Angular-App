89bb02630f86d50b936722eee6cc44b6482dd389
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-07-21 16:39:18 +0200

    implement function to get all the favorite movies

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
-  styleUrl: './profile-view.component.scss'
+  styleUrl: './profile-view.component.scss',
 })
 export class ProfileViewComponent {
+  favoriteMovies: any[] = [];
+  filteredFavoriteMovies = [];
 
+  constructor(public fetchApiData: FetchApiDataService) {}
+
+  ngOnInit(): void {
+    this.getFavoriteMovies();
+  }
+
+  //function to get all the favourite movies
+  getFavoriteMovies(): void {
+    const savedUser = localStorage.getItem('user');
+    console.log(savedUser);
+    const userId = JSON.parse(savedUser)._id;
+    console.log(userId);
+
+    const savedMovies = localStorage.getItem('movies');
+    console.log(savedMovies);
+    const movies = JSON.parse(savedMovies);
+    console.log(movies);
+
+    this.fetchApiData.getUser(userId).subscribe((resp: any) => {
+      this.favoriteMovies = resp.favoriteMovies;
+
+      this.favoriteMovies.forEach((favoriteMovie) => {
+        movies.forEach((movie) => {
+          if (favoriteMovie === movie._id) {
+            this.filteredFavoriteMovies.push(movie);
+          }
+        });
+      });
+
+      console.log(this.filteredFavoriteMovies);
+
+      // console.log(resp);
+    });
+  }
 }
