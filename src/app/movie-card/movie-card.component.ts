de67fe12d0555aa9e27224aa1542d3faeca0563b
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-07-21 15:47:21 +0200

    implement a dialog when the director button is clicked

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DirectorInfoComponent } from '../director-info/director-info.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,7 +11,23 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 export class MovieCardComponent {
   //movies returned from the API
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) {}
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog
+  ) {}
+
+  // function that will open the dialog when the director button is clicked
+  openDirectorInfoDialog(movie: any): void {
+    // console.log(movie);
+    const director = movie.Director;
+    console.log(director);
+
+    this.dialog.open(DirectorInfoComponent, {
+      width: '280px',
+      data: { director },
+    });
+  }
 
   ngOnInit(): void {
     this.getMovies();
@@ -19,8 +37,10 @@ export class MovieCardComponent {
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
+      //save all the movies to local storage
+      localStorage.setItem('movies', JSON.stringify(resp));
       console.log(this.movies);
-      return this.movies;
+      // return this.movies;
     });
   }
 }
