c1eec7e93eed38f427df3434eec25c115b5fe6c5
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-07-22 22:56:54 +0200

    implement logic for reactive forms; add navigation to movies

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+// import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-profile-view',
@@ -8,13 +10,33 @@ import { Router } from '@angular/router';
   styleUrl: './profile-view.component.scss',
 })
 export class ProfileViewComponent {
+  // items = this.cartService.getItems();
+  checkoutForm: any;
+
   favoriteMovies: any[] = [];
   filteredFavoriteMovies = [];
+  // @Input({ required: true }) userDetails: any;
 
   constructor(
     public fetchApiData: FetchApiDataService,
-    private router: Router
-  ) {}
+    private router: Router,
+    // private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {
+    this.checkoutForm = this.formBuilder.group({
+      Name: '',
+      Password: '',
+      Email: '',
+      Birthday: '',
+    });
+  }
+
+  onSubmit(): void {
+    // this.items = this.cartService.clearCart();
+    console.log('Data submitted: ', this.checkoutForm.value);
+    this.updateProfile(this.checkoutForm.value);
+    // this.checkoutForm.reset();
+  }
 
   ngOnInit(): void {
     this.getFavoriteMovies();
@@ -46,6 +68,19 @@ export class ProfileViewComponent {
       console.log(this.filteredFavoriteMovies);
 
       // console.log(resp);
+    });
+  }
+
+  onMovieRemoved(movieId: any): void {
+    this.filteredFavoriteMovies = this.filteredFavoriteMovies.filter(
+      //filter method creates a new array with all elements that pass test
+      (movie) => movie._id !== movieId
+    );
+  }
+
+  updateProfile(userDetails): void {
+    this.fetchApiData.updateUser(userDetails).subscribe((response) => {
+      console.log('user has been updated: ', userDetails);
     });
   }
 
